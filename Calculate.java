bf9a8878e9c24b8f243f13aa2776c43183e90110
KorsyukAO <korsyuk.ao@gmail.com>
2018-01-10 22:06:28 +0300

    create Calculate2

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,19 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Hello word");
-	
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		
+		int resultAddition = first + second;
+		int resultSubsraction = first - second;
+		int resultMultiplicaton = first * second;
+		int resultDivision = first / second;
+		
+		System.out.println("result " + resultAddition );
+		System.out.println("result " + resultSubsraction);
+		System.out.println("result " + resultMultiplicaton);
+		System.out.println("result " + resultDivision);
+		
 	}
 	
 }
